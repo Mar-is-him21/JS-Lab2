4216c274e170ea93217e69a36be11740e3e4d8f9
Mar-is-him21 <osbornem983@my.clevelandcc.edu>
2024-12-06 10:08:11 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function changeFont() {
+    document.getElementById("body").style.fontFamily = "sans-serif";
+  }
+  
+  function turnBlue() {
+  document.getElementById("a").style.color = "Blue";
+  }
+  
+  function turnRed() {
+  document.getElementById("a").style.color = "Red";
+  }
+  
+  function turnBlack() {
+  document.getElementById("a").style.color = "Black";
+  }
+
